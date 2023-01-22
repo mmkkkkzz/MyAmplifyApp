9914ffb76f91a1de083a5d9e5cddb5e81c8d95d5
mmkkkkzz <117556731+mmkkkkzz@users.noreply.github.com>
2023-01-23 06:17:05 +0900

    Initial Commit

--- MyAmplifyApp/MyAmplifyAppApp.swift
+++ MyAmplifyApp/MyAmplifyAppApp.swift
@@ -4,11 +4,28 @@
 //
 //  Created by 三木一真 on 2023/01/23.
 //
-
+import Amplify
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        do {
+            Amplify.Logging.logLevel = .verbose
+            try Amplify.configure()
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
+        return true
+    }
+}
 
 @main
 struct MyAmplifyAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myamplifyappc1e60180": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
