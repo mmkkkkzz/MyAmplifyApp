408dd6dd9a0a062ed6bbb70e258406e020703e0c
mmkkkkzz <117556731+mmkkkkzz@users.noreply.github.com>
2023-01-23 04:13:46 +0900

    Initial Commit

---
+++ MyAmplifyApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyAmplifyApp
+//
+//  Created by 三木一真 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAmplifyApp/MyAmplifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmplifyAppApp.swift
+//  MyAmplifyApp
+//
+//  Created by 三木一真 on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAmplifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
